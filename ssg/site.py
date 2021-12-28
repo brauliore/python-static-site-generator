e49e47bbb6431838e0c5dc23091cfe188b90c67c
Braulio Rivera Espinoza <brauliore@gmail.com>
2021-12-27 19:53:01 -0600

    update

--- ssg/site.py
+++ ssg/site.py
@@ -21,7 +21,7 @@ class Site:
         if parser is not None:
             parser.parse(path, self.source, self.dest)
         else: 
-            self.error("No parser for the {} extension, file skipped".format(path.suffix))
+            self.error("No parser for the {} extension, file skipped!".format(path.suffix))
 
     def build(self):
         self.dest.mkdir(parents=True, exist_ok=True)
@@ -33,4 +33,4 @@ class Site:
 
     @staticmethod
     def error(message):
-        sys.stderr.write("\x1b[1;32m{}\n".format(message))
+        sys.stderr.write("\x1b[1;31m{}\n".format(message))
